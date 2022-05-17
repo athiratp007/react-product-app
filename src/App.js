7d95df991b409869c238b7c977e5973d4a66c46e
athiratp007 <athiratp007@example.com>
2022-05-18 00:06:48 +0530

    srch

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import ProductEntry from './Components/ProductEntry';
+import SearchProduct from './Components/SearchProduct';
 
 function App() {
   return (
     <div>
 <ProductEntry/>
+<SearchProduct/>
     </div>
     
   );
